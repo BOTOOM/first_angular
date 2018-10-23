322085762e02bc8ecc86931212534ebd34ce3e88
BOTOOM <eddiazr@correo.udistrital.edu.co>
2018-10-22 23:52:40 -0500

    primer comopente de prueba--hola mundo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { helloWorld } from './primercomponente/primer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    helloWorld
   ],
   imports: [
     BrowserModule,
